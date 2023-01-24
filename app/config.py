0f81bb113679d32c05c64ad3e03f0abf64e6186f
Sarah <slshimizu92@gmail.com>
2023-01-24 06:15:52 -0500

    project walk-through

--- app/config.py
+++ app/config.py
@@ -8,8 +8,7 @@ This file contains all configurables of our app.
 config = {
     "model_weight": "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/hotdog-not-hotdog/data"
                     "/model_weight.pt",
+    "project_name": "Is it a Hot Dog?",
     # TODO: Change it!
-    "project_name": "SAMPLE PROJECT NAME",
-    # TODO: Change it!
-    "project_description": "SAMPLE PROJECT DESCRIPTION"
+    "project_description": "Image detection and classifier of hot dogs"
 }
